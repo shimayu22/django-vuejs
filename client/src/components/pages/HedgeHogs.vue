2dd1622ce1370e0e6e54e5cfa006bcb62377ba84
Yutaka Shimada <shimayu22@gmail.com>
2020-07-11 00:26:04 +0900

    ログイン画面の作成

---
+++ client/src/components/pages/HedgeHogs.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    HedgeHog Page
+  </div>
+</template>
+
+<script>
+import router from "../../router";
+export default {
+  name: "HedgeHogs",
+  mounted() {
+    this.checkLoggedIn();
+  },
+  methods: {
+    checkLoggedIn() {
+      router.push('/auth')
+    }
+  }
+};
+</script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,22 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+import Auth from '../components/pages/Auth'
+import HedgeHogs from '../components/pages/HedgeHogs'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'HedgeHogs',
+    component: HedgeHogs
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/auth',
+    name: 'Auth',
+    component: Auth
+    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
